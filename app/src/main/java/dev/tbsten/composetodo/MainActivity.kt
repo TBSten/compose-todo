5079f2242aa856d8e4ebff90446c98da1854f508
TBSten <programmingcafeteria@gmail.com>
2023-01-04 14:11:53 +0900

    HomeScreenを追加

--- app/src/main/java/dev/tbsten/composetodo/MainActivity.kt
+++ app/src/main/java/dev/tbsten/composetodo/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dev.tbsten.composetodo.home.HomeScreen
 import dev.tbsten.composetodo.ui.theme.ComposeTodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +18,7 @@ class MainActivity : ComponentActivity() {
       ComposeTodoTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          AppRoot()
         }
       }
     }
@@ -27,14 +26,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun AppRoot() {
+  HomeScreen()
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  ComposeTodoTheme {
-    Greeting("Android")
-  }
-}
---
+++ app/src/main/java/dev/tbsten/composetodo/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package dev.tbsten.composetodo.home
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+  Text("home screen")
+}
