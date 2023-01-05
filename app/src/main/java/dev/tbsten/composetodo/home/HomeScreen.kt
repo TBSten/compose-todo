77bdfc4edf08a3f3cff7e5d78a5af383ffd1b63e
TBSten <programmingcafeteria@gmail.com>
2023-01-05 14:30:47 +0900

    HomeScreenからTodoListItemを切り出し、Previewを追加

--- app/src/main/java/dev/tbsten/composetodo/home/HomeScreen.kt
+++ app/src/main/java/dev/tbsten/composetodo/home/HomeScreen.kt
@@ -1,12 +1,22 @@
 package dev.tbsten.composetodo.home
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.tbsten.composetodo.domain.Todo
+import java.time.LocalDateTime
 
 @Composable
 fun HomeScreen(
@@ -33,7 +43,7 @@ fun HomeScreen(
         }
         // todoがnullじゃないならそれを一覧表示
         items(_todos) { todo ->
-          Text("${todo.title}")
+          TodoListItem(todo)
         }
       } else {
         // todosがnullならロード中
@@ -49,3 +59,35 @@ fun HomeScreen(
 fun HomeTopBar() {
   TopAppBar { Text("ホーム") }
 }
+
+@Composable
+fun TodoListItem(
+  todo: Todo,
+) {
+  Row(
+    modifier = Modifier.fillMaxWidth(),
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
+    Text("${todo.title}", modifier = Modifier.weight(1F))
+    IconButton(onClick = { TODO("not implement") }) {
+      Icon(rememberVectorPainter(Icons.Default.Delete), "削除")
+    }
+  }
+}
+
+@Preview(widthDp = 300)
+@Preview(widthDp = 200)
+@Preview(widthDp = 100)
+@Composable
+fun TodoListItemPreview() {
+  val exampleTodo = Todo(
+    id = 0,
+    title = "test",
+    details = "test test test",
+    createAt = LocalDateTime.now(),
+    updateAt = LocalDateTime.now(),
+  )
+
+  TodoListItem(exampleTodo)
+}
+
